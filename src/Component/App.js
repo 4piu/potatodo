5709a77d62f2709892c956f0a2065aa97bfc43c7
4piu <16668056@users.noreply.github.com>
2022-03-07 01:18:38 +0800

    Add active task id to context

--- src/Component/App.js
+++ src/Component/App.js
@@ -6,13 +6,18 @@ import Appbar from "./Appbar";
 import BottomNav from "./BottomNav";
 import ActivityContext from "../Context/ActivityContext";
 import {Container} from "@mui/material";
+import TaskList from "./TaskList";
+import TaskDetail from "./TaskDetail";
+import Statistics from "./Statistics";
+import TimerView from "./TimerView";
 
 class App extends PureComponent {
     constructor(props) {
         super(props);
 
         this.state = {
-            activity: ActivityContext.Activity.TASK
+            activity: ActivityContext.Activity.TASK,
+            activeTaskId: null
         };
     }
 
@@ -22,19 +27,34 @@ class App extends PureComponent {
         });
     };
 
+    setActiveTaskId = (newVal) => {
+        this.setState({
+            activeTaskId: newVal
+        });
+    };
+
     render() {
 
         return (
             <ActivityContext.Provider value={{
                 activity: this.state.activity,
-                setActivity: this.setActivity
+                setActivity: this.setActivity,
+                activeTaskId: this.state.activeTaskId,
+                setActiveTaskId: this.setActiveTaskId
             }}>
                 <CssBaseline/>
                 {(this.state.activity !== ActivityContext.Activity.TIMER) &&
                     <Appbar/>}
                 <Container maxWidth="lg">
-
+                    {this.state.activity === ActivityContext.Activity.TASK &&
+                        <TaskList/>}
+                    {this.state.activity === ActivityContext.Activity.DETAIL &&
+                        <TaskDetail/>}
+                    {this.state.activity === ActivityContext.Activity.STATISTICS &&
+                        <Statistics/>}
                 </Container>
+                {this.state.activity === ActivityContext.Activity.TIMER &&
+                    <TimerView/>}
                 {(this.state.activity === ActivityContext.Activity.TASK ||
                         this.state.activity === ActivityContext.Activity.STATISTICS) &&
                     <BottomNav/>}
