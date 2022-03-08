af7187fdffe05b66055df6fecd305bcb8e9bee83
4piu <16668056@users.noreply.github.com>
2022-03-08 22:59:19 +0800

    Cache local storage

--- src/Utility/StorageHelper.js
+++ src/Utility/StorageHelper.js
@@ -1,31 +1,31 @@
 import {Task} from "./Task";
 
 class StorageHelper {
+    static cacheAllTask = null;
+
     static getAllTask = () => {
-        const taskMap = JSON.parse(localStorage.getItem("task")) || {};
+        StorageHelper.cacheAllTask = StorageHelper.cacheAllTask || JSON.parse(localStorage.getItem("task")) || {};
         const ret = [];
-        for (const [_, v] of Object.entries(taskMap)) {
+        for (const [_, v] of Object.entries(StorageHelper.cacheAllTask)) {
             ret.push(new Task(v));
         }
         return ret;
     };
 
     static getTask = (taskId) => {
-        const taskMap = JSON.parse(localStorage.getItem("task")) || {};
-        const options = taskMap[taskId];
+        StorageHelper.cacheAllTask = StorageHelper.cacheAllTask || JSON.parse(localStorage.getItem("task")) || {};
+        const options = StorageHelper.cacheAllTask[taskId];
         return options && new Task(options);
     };
 
     static updateTask = (taskId, options) => {
-        const taskMap = JSON.parse(localStorage.getItem("task")) || {};
-        taskMap[taskId] = options;
-        localStorage.setItem('task', JSON.stringify(taskMap));
+        StorageHelper.cacheAllTask[taskId] = options;
+        localStorage.setItem('task', JSON.stringify(StorageHelper.cacheAllTask));
     };
 
     static addTask = (task) => {
-        const taskMap = JSON.parse(localStorage.getItem("task")) || {};
-        taskMap[task.uuid] = task;
-        localStorage.setItem('task', JSON.stringify(taskMap));
+        StorageHelper.cacheAllTask[task.uuid] = task;
+        localStorage.setItem('task', JSON.stringify(StorageHelper.cacheAllTask));
     };
 }
 
