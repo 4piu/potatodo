de461ef749ec6bcb9b285a03886699816014ed40
4piu <16668056@users.noreply.github.com>
2022-03-09 05:55:50 +0000

    Add Prompt component

---
+++ src/Component/Prompt.js
@@ -0,0 +1,33 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { PureComponent } from "react";
+
+class Prompt extends PureComponent {
+    render() {
+        return (
+            <Dialog
+                open={this.props.open}
+                onClose={this.props.onClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {this.props.title}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {this.props.content}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    {this.props.actions.map((item, idx) => (
+                        <div key={idx}>
+                           {item}
+                        </div>
+                    ))}
+                </DialogActions>
+            </Dialog>
+        );
+    }
+}
+
+export default Prompt;
