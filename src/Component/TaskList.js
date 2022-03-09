89f51d82f1f3f252b093fc192c022afcf6618579
4piu <16668056@users.noreply.github.com>
2022-03-09 05:58:10 +0000

    Fix item index in TaskList

--- src/Component/TaskList.js
+++ src/Component/TaskList.js
@@ -7,8 +7,8 @@ class TaskList extends PureComponent {
         return (
             <>
                 <Grid container spacing={{xs: 2, md: 4}}>
-                    {this.props.taskList.map((item, index) => (
-                        <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+                    {this.props.taskList.map(item => (
+                        <Grid item key={item.uuid} xs={12} sm={6} md={4} lg={3}>
                             <TaskCard task={item}/>
                         </Grid>
                     ))}
