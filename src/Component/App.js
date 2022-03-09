769f0ca4444b6f039f1136929bf4e0d2bc4af210
4piu <16668056@users.noreply.github.com>
2022-03-09 05:12:19 +0000

    Clean conditional layout in App.js

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import CssBaseline from '@mui/material/CssBaseline';
-import {PureComponent} from "react";
+import { PureComponent } from "react";
 import Appbar from "./Appbar";
 import BottomNav from "./BottomNav";
 import ActivityContext from "../Context/ActivityContext";
-import {Container, Fab} from "@mui/material";
+import { Container, Fab } from "@mui/material";
 import TaskList from "./TaskList";
 import TaskDetail from "./TaskDetail";
 import Statistics from "./Statistics";
@@ -51,6 +51,10 @@ class App extends PureComponent {
     };
 
     render() {
+        const isTaskView = this.state.activity === ActivityContext.Activity.TASK,
+            isDetailView = this.state.activity === ActivityContext.Activity.DETAIL,
+            isStatisticsView = this.state.activity === ActivityContext.Activity.STATISTICS,
+            isTimerView = this.state.activity === ActivityContext.Activity.TIMER;
 
         return (
             <ActivityContext.Provider value={{
@@ -59,28 +63,29 @@ class App extends PureComponent {
                 activeTaskId: this.state.activeTaskId,
                 setActiveTaskId: this.setActiveTaskId
             }}>
-                <CssBaseline/>
-                {(this.state.activity !== ActivityContext.Activity.TIMER) &&
-                    <Appbar/>}
-                <Container maxWidth="lg" sx={{py: 11, backgroundColor: '#eee', minHeight: '100vh'}}>
-                    {this.state.activity === ActivityContext.Activity.TASK &&
-                        <TaskList taskList={StorageHelper.getAllTask()}/>}
-                    {this.state.activity === ActivityContext.Activity.DETAIL &&
-                        <TaskDetail/>}
-                    {this.state.activity === ActivityContext.Activity.STATISTICS &&
-                        <Statistics/>}
-                </Container>
-                {this.state.activity === ActivityContext.Activity.TIMER &&
-                    <TimerView/>}
-                {(this.state.activity === ActivityContext.Activity.TASK ||
-                        this.state.activity === ActivityContext.Activity.STATISTICS) &&
-                    <BottomNav/>}
+                <CssBaseline />
+                {!isTimerView &&
+                    <>
+                        <Appbar />
+                        <Container maxWidth="lg" sx={{ py: 11, backgroundColor: '#eee', minHeight: '100vh' }}>
+                            {isTaskView &&
+                                <TaskList taskList={StorageHelper.getAllTask()} />}
+                            {isStatisticsView &&
+                                <Statistics />}
+                            {isDetailView &&
+                                <TaskDetail />}
+                        </Container>
+                        {!isDetailView &&
+                            <BottomNav />}
+                    </>}
+                {isTimerView &&
+                    <TimerView />}
                 {this.state.showTaskEditor &&
-                    <TaskEditor onClose={this.editorClose} activeTaskId={this.state.activeTaskId}/>}
-                {this.state.activity === ActivityContext.Activity.TASK &&
+                    <TaskEditor onClose={this.editorClose} activeTaskId={this.state.activeTaskId} />}
+                {isTaskView &&
                     <Fab color="secondary" aria-label="add" onClick={this.fabClick}
-                         sx={{position: 'fixed', bottom: 80, right: 30}}>
-                        <AddIcon/>
+                        sx={{ position: 'fixed', bottom: 80, right: 30 }}>
+                        <AddIcon />
                     </Fab>}
 
             </ActivityContext.Provider>
